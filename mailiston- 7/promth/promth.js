46428be05763fdd64ffb9081a4984a4986c1da2e
mehedi37m <mz.mehedi37@gmail.com>
2023-09-18 14:17:53 +0600

    all folder

---
+++ mailiston- 7/promth/promth.js
@@ -0,0 +1,13 @@
+const seriousStudent = () => {
+    const student = confirm('are you sure you want to');
+    if (student === true){
+        alert('welcome');
+    }else{
+        alert('do not worry');
+    }
+}
+
+const StudentName = () => {
+    const name = prompt('what your name is');
+    
+}
